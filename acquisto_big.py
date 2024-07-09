c099e1dce22c7482cb42abee4807a01cd84beed7
danishindryas <indryasdanish83@gmail.com>
2024-07-09 20:01:35 +0200

    Update acquisto_big.py
    bug fix

--- acquisto_big.py
+++ acquisto_big.py
@@ -10,18 +10,18 @@ db = client['MongoTicket']
 events = db['events']
 tickets = db['tickets']
 
-# Funzione per generare un biglietto
-def generate_ticket(concert_name):
-    event = events.find_one({"concert_name": concert_name})  # Trova il concerto nel DB
-    if not event:
-        return "Concerto non trovato"
-    else:
+def generate_ticket(concert_name, num):
+    event = events.find_one({"concert_name": concert_name})
+    if event:
         if event['places_available'] <= 0:
-            return "Biglietti esauriti"
+            return False
         else:
-            ticket_id = str(uuid.uuid4()) # uuid serve per generare un ID univoco per ogni biglietto
-            ticket = {"ticket_id": ticket_id, "concert_name": concert_name, "seat_number": event['places_available']}
-            
-            tickets.insert_one(ticket)  # Inserisci il biglietto nel database
-            events.update_one({"concert_name": concert_name}, {"$inc": {"places_available": -1}})  # Aggiorna i posti disponibili nel concerto
-            return ticket
+            while num >0:
+                events.update_one({"concert_name": concert_name},
+                                  {"$inc": {"places_available": -1}})
+                ticket = {"concert_name": concert_name,
+                          "seat_number": event['places_available']-num}
+                receipt = tickets.insert_one(ticket)
+                num -= 1
+                print(f"Ticket ID: {receipt.inserted_id}")
+            return True
