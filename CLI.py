d99583152490af55e54c2fa3b1ed31dc466f9ca4
adish-rmr <168539641+adish-rmr@users.noreply.github.com>
2024-07-17 00:14:21 +0200

    rewrite of all the code, eliminate functions, makeover of cli, testing, bug fixing

--- CLI.py
+++ CLI.py
@@ -1,5 +1,4 @@
 import pymongo
-from unidecode import unidecode
 from config import uri
 from datetime import datetime
 import time
@@ -14,37 +13,17 @@ tickets = db['tickets']
 # Crea indice geospaziale
 events.create_index([("stage.location", pymongo.GEOSPHERE)])
 
+
 # Funzioni di ricerca
-def search_by_name():
-    keyword = input("Inserisci il nome del concerto (o 'q' per tornare indietro): ")
-    if keyword.lower() == 'q':
-        return None
+def search_by_name(keyword):
     return list(events.find({"concert_name": {"$regex": keyword, "$options": "i"}}))
 
-def search_by_artist():
-    keyword = input("Inserisci il nome dell'artista (o 'q' per tornare indietro): ")
-    if keyword.lower() == 'q':
-        return None
+
+def search_by_artist(keyword):
     return list(events.find({"artists": {"$regex": keyword, "$options": "i"}}))
 
-def search_by_date():
-    try:
-        start_date = input("Inserisci la data di inizio (YYYY-MM-DD) (o 'q' per tornare indietro): ")
-        if start_date.lower() == 'q':
-            return None
-        end_date = input("Inserisci la data di fine (YYYY-MM-DD) (o 'q' per tornare indietro): ")
-        if end_date.lower() == 'q':
-            return None
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        
-        if end_date < start_date:
-            print("Errore: La data di fine non può essere precedente alla data di inizio.")
-            return []
-    except ValueError:
-        print("Formato data non valido. Usa il formato YYYY-MM-DD.")
-        return []
 
+def search_by_date(start_date,end_date):
     query = {
         "date": {
             "$gte": start_date,
@@ -53,7 +32,7 @@ def search_by_date():
     }
     return list(events.find(query))
 
-# Funzioni di geolocalizzazione
+
 def get_coordinates(location_name):
     geolocator = Photon(user_agent="geoapiExercises")
     location = geolocator.geocode(location_name)
@@ -62,6 +41,7 @@ def get_coordinates(location_name):
     else:
         print(f"Impossibile trovare le coordinate per {location_name}")
         return None
+
 
 def search_by_location(location_name):
     coordinates = get_coordinates(location_name)
@@ -84,69 +64,47 @@ def search_by_location(location_name):
     else:
         return None
 
-# Funzioni di gestione biglietti
-def calculate_total_cost(num_tickets, price_per_ticket):
-    return num_tickets * price_per_ticket
 
-def generate_ticket(event_id, concert_name, num, name_buyer, price_per_ticket):
-    event = events.find_one({"_id": event_id})
+def generate_ticket(event, num, name_buyer):
     if event:
         if event['places_available'] < num:
             print("Non ci sono abbastanza posti disponibili.")
         else:
             for i in range(num):
-                events.update_one({"_id": event_id}, {"$inc": {"places_available": -1}})
+                events.update_one({"_id": event['_id']}, {"$inc": {"places_available": -1}})
                 ticket = {
                     "id_event": event["_id"],
                     "concert_name": event["concert_name"],
                     "buyer_name": name_buyer,
                     "seat_number": event['places_available'] - i,
-                    "price": price_per_ticket
+                    "price": event['price']
                 }
                 receipt = tickets.insert_one(ticket)
                 print(f"Ticket ID: {receipt.inserted_id}")
-            print(f"{num} biglietti generati con successo per {concert_name}")
+            print(f"{num} biglietti generati con successo per {event['concert_name']}")
+    else:
+        return None
 
-# Funzioni di utilità
-def clear_screen():
-    print("\033[H\033[J", end="")
+
 
 def display_events(results):
     for i, event in enumerate(results):
-        print(f"{i + 1}. {event.get('concert_name', 'N/A')} - Artisti: {event.get('artists', 'N/A')} - Data: {event.get('date', 'N/A')} - Posti disponibili: {event.get('places_available', 'N/A')} - Prezzo: €{event.get('price', 'N/A')}")
+        print(f"{i + 1}. {event['concert_name']} \n"
+              f"Artisti: {event['artists']} \n"
+              f"Data: {event['date']} \n"
+              f"Posti disponibili: {event['places_available']} \n"
+              f"Prezzo: €{event['price']} \n")
 
-def process_event_selection(results):
-    try:
-        selection = input("Seleziona l'evento desiderato inserendo il numero corrispondente (o 'q' per tornare indietro): ")
-        if selection.lower() == 'q':
-            return None
-        selection = int(selection) - 1
-        if 0 <= selection < len(results):
-            return results[selection]
-        else:
-            print("Selezione non valida.")
-            return None
-    except ValueError:
-        print("Errore: inserisci un numero valido.")
-        return None
 
-def process_ticket_purchase(event):
-    concert_name = event.get('concert_name', 'N/A')
-    num = int(input(f"Quanti biglietti desideri acquistare per {concert_name}? "))
-    price_per_ticket = event.get('price', 0)
-    total_cost = calculate_total_cost(num, price_per_ticket)
-    print(f"Il costo totale è di €{total_cost:.2f}")
-    confirm = input("Vuoi procedere con l'acquisto? (s/n): ").lower()
-    if confirm != 's':
-        print("Acquisto annullato.")
-        return
-    name_buyer = input("Inserisci il nome dell'acquirente: ")
-    generate_ticket(event["_id"], concert_name, num, name_buyer, price_per_ticket)
+def ticket_cost(event, num):
+    price_per_ticket = event['price']
+    total_cost = num * price_per_ticket
+    return total_cost
 
-# Funzione principale
+
+# CLI
 def avvia_sessione():
     while True:
-        clear_screen()
         print("Opzioni disponibili:")
         print("1. Cerca per nome del concerto")
         print("2. Cerca per artista")
@@ -156,33 +114,86 @@ def avvia_sessione():
         scelta = input("Inserisci il numero dell'opzione desiderata: ")
 
         results = None
+
         if scelta == '1':
-            results = search_by_name()
+            keyword = input("Inserisci il nome del concerto: ")
+            if keyword.lower() == 'q' or len(keyword) == 0 or len(keyword) > 30:
+                pass
+            else:
+                results = search_by_name(keyword)
+
         elif scelta == '2':
-            results = search_by_artist()
+            keyword = input("Inserisci il nome dell'artista: ")
+            if keyword.lower() == 'q' or len(keyword) == 0 or len(keyword) > 30:
+                pass
+            else:
+                results = search_by_artist(keyword)
+
         elif scelta == '3':
-            location_name = input("Inserisci il nome della località (o 'q' per tornare indietro): ")
-            if location_name.lower() != 'q':
+            location_name = input("Inserisci il nome della località: ")
+            if location_name.lower() == 'q' or len(location_name) == 0 or len(location_name) > 30 or len(location_name) < 3:
+                pass
+            else:
                 results = search_by_location(location_name)
+
         elif scelta == '4':
-            results = search_by_date()
+            try:
+                start_date = input("Inserisci la data di inizio (YYYY-MM-DD) (o 'q' per tornare indietro): ")
+                end_date = input("Inserisci la data di fine (YYYY-MM-DD) (o 'q' per tornare indietro): ")
+                if 2023 < int(start_date[:4]) < 2031 and 0 < int(start_date[5:7]) < 13 and 0 < int(start_date[8:10]) < 32:
+                    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+                    if 2023 < int(end_date[:4]) < 2031 and 0 < int(end_date[5:7]) < 13 and 0 < int(end_date[8:10]) < 32:
+                        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+                        if end_date > start_date:
+                            results = search_by_date(start_date, end_date)
+                        else:
+                            print("Errore: La data di fine non può essere precedente alla data di inizio.")
+                            pass
+                    else:
+                        pass
+                else:
+                    pass
+            except ValueError:
+                print("Formato data non valido. Usa il formato YYYY-MM-DD.")
+                pass
+
         elif scelta == '5':
             print("Grazie per aver usato l'applicazione.")
             break
         else:
             print("Opzione non valida. Riprova.")
-            time.sleep(2)
+            time.sleep(1)
             continue
 
         if results:
             display_events(results)
-            selected_event = process_event_selection(results)
-            if selected_event:
-                process_ticket_purchase(selected_event)
+            selection = int(input("Seleziona l'evento desiderato inserendo il numero corrispondente: "))
+            if 1 <= selection <= len(results):
+                selection -= 1
+                selected_event = results[selection]
+                if selected_event:
+                    num = int(input(f"Quanti biglietti desideri acquistare per l'evento? "))
+                    if num > 0 and isinstance(num, int):
+                        total_cost = ticket_cost(selected_event, num)
+                        print(f"Il costo totale è di €{total_cost:.2f}")
+                        confirm = input("Vuoi procedere con l'acquisto? (s/n): ").lower()
+                        if confirm == 's' or confirm == 'S':
+                            name_buyer = input("Inserisci il nome dell'acquirente: ")
+                            if name_buyer.lower() != 'q' and len(name_buyer) > 0:
+                                generate_ticket(selected_event, num, name_buyer)
+                            else:
+                                pass
+                        else:
+                            pass
+                    else:
+                        pass
+                else:
+                    pass
+            else:
+                pass
         elif results is not None:
             print("Nessun evento trovato.")
 
-        input("Premi Invio per continuare...")
 
 if __name__ == "__main__":
     avvia_sessione()
